1a8d6597a4c9d84a314550dde0733788e7721541
seorsum1 <github@seorsum.xyz>
2023-04-30 14:06:11 -0500

    adjusted spacing for readability

--- mainUI.py
+++ mainUI.py
@@ -5,6 +5,7 @@ class Ui_mainWindow(object):
     def setupUi(self, mainWindow):
         mainWindow.setObjectName("mainWindow")
         mainWindow.resize(500, 750)
+        
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -12,54 +13,70 @@ class Ui_mainWindow(object):
         mainWindow.setSizePolicy(sizePolicy)
         mainWindow.setMinimumSize(QtCore.QSize(500, 750))
         mainWindow.setMaximumSize(QtCore.QSize(500, 750))
+        
         self.centralwidget = QtWidgets.QWidget(mainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.mainLabel = QtWidgets.QLabel(self.centralwidget)
         self.mainLabel.setGeometry(QtCore.QRect(175, 30, 150, 30))
+        
         font = QtGui.QFont()
         font.setPointSize(16)
+        
         self.mainLabel.setFont(font)
         self.mainLabel.setScaledContents(False)
         self.mainLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.mainLabel.setObjectName("mainLabel")
+        
         self.shopButton = QtWidgets.QPushButton(self.centralwidget)
         self.shopButton.setGeometry(QtCore.QRect(200, 150, 100, 40))
         self.shopButton.setObjectName("shopButton")
+        
         self.doneButton = QtWidgets.QPushButton(self.centralwidget)
         self.doneButton.setGeometry(QtCore.QRect(375, 645, 100, 40))
         self.doneButton.setObjectName("doneButton")
+        
         self.resetButton = QtWidgets.QPushButton(self.centralwidget)
         self.resetButton.setGeometry(QtCore.QRect(25, 645, 100, 40))
         self.resetButton.setObjectName("resetButton")
+        
         self.cartButton = QtWidgets.QPushButton(self.centralwidget)
         self.cartButton.setGeometry(QtCore.QRect(200, 250, 100, 40))
         self.cartButton.setObjectName("cartButton")
+        
         self.outputLabel = QtWidgets.QLabel(self.centralwidget)
         self.outputLabel.setGeometry(QtCore.QRect(50, 475, 400, 150))
         self.outputLabel.setText("")
         self.outputLabel.setObjectName("outputLabel")
+        
         self.cookieButton = QtWidgets.QPushButton(self.centralwidget)
         self.cookieButton.setGeometry(QtCore.QRect(350, 150, 100, 40))
         self.cookieButton.setObjectName("cookieButton")
+        
         self.sandwichButton = QtWidgets.QPushButton(self.centralwidget)
         self.sandwichButton.setGeometry(QtCore.QRect(350, 250, 100, 40))
         self.sandwichButton.setObjectName("sandwichButton")
+        
         self.waterButton = QtWidgets.QPushButton(self.centralwidget)
         self.waterButton.setGeometry(QtCore.QRect(350, 350, 100, 40))
         self.waterButton.setObjectName("waterButton")
+        
+        font.setPointSize(12)
+        
         self.cookieLabel = QtWidgets.QLabel(self.centralwidget)
         self.cookieLabel.setGeometry(QtCore.QRect(40, 150, 130, 40))
-        font.setPointSize(12)
         self.cookieLabel.setFont(font)
         self.cookieLabel.setObjectName("cookieLabel")
+        
         self.sandwichLabel = QtWidgets.QLabel(self.centralwidget)
         self.sandwichLabel.setGeometry(QtCore.QRect(40, 250, 150, 40))
         self.sandwichLabel.setFont(font)
         self.sandwichLabel.setObjectName("sandwichLabel")
+        
         self.waterLabel = QtWidgets.QLabel(self.centralwidget)
         self.waterLabel.setGeometry(QtCore.QRect(40, 350, 150, 40))
         self.waterLabel.setFont(font)
         self.waterLabel.setObjectName("waterLabel")
+        
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(mainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 20))
