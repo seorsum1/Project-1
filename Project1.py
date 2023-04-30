d88c15399d53dd8178c5b68c786e0d30c5a98d48
seorsum1 <github@seorsum.xyz>
2023-04-30 14:02:12 -0500

    Added docstrings and type hints, remove extra func

--- Project1.py
+++ Project1.py
@@ -1,17 +1,17 @@
 import sys
 from PyQt5 import QtWidgets
 from mainUI import Ui_mainWindow
-
+from typing import Dict
 
 class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
         self.setupUi(self)
-        self.cart = {"cookie": 0, "sandwich": 0, "water": 0}
-        self.prices = {"cookie": 1.5, "sandwich": 4, "water": 1}
+        self.cart: Dict[str, int] = {"cookie": 0, "sandwich": 0, "water": 0}
+        self.prices: Dict[str, float] = {"cookie": 1.5, "sandwich": 4, "water": 1}
 
-        self.init_visibility()
+        self.hide_shop_items()
 
         self.shopButton.clicked.connect(self.show_shop_items)
         self.doneButton.clicked.connect(self.hide_shop_items)
@@ -21,19 +21,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
         self.sandwichButton.clicked.connect(lambda: self.add_item("sandwich"))
         self.waterButton.clicked.connect(lambda: self.add_item("water"))
 
-    def init_visibility(self):
-        self.cookieButton.setVisible(False)
-        self.sandwichButton.setVisible(False)
-        self.waterButton.setVisible(False)
-        self.cookieLabel.setVisible(False)
-        self.sandwichLabel.setVisible(False)
-        self.waterLabel.setVisible(False)
-        self.doneButton.setVisible(False)
 
-    def close_app(self):
-        self.close()
+    def add_item(self, item: str) -> None:
+        """
+        Add an item to the cart and update the output label.
 
-    def add_item(self, item):
+        Args:
+            item (str): The item to be added to the cart.
+        """
         self.cart[item] += 1
         if self.cart[item] == 1:
             self.outputLabel.setText(f"Added {self.cart[item]} {item.capitalize()}")
@@ -43,14 +38,23 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
             self.outputLabel.setText(f"Added {self.cart[item]} {item.capitalize()}s")
         self.outputLabel.repaint()
 
-    def show_cart(self):
+    def show_cart(self) -> None:
+        """
+        Update the output label to display the cart contents.
+        """
         self.update_output_label()
 
-    def reset_cart(self):
+    def reset_cart(self) -> None:
+        """
+        Reset the cart and update the output label.
+        """
         self.cart = {"cookie": 0, "sandwich": 0, "water": 0}
         self.update_output_label()
 
-    def show_shop_items(self):
+    def show_shop_items(self) -> None:
+        """
+        Show shop items and their respective buttons and labels.
+        """
         self.shopButton.setVisible(False)
         self.cartButton.setVisible(False)
         self.cookieButton.setVisible(True)
@@ -63,7 +67,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
         self.outputLabel.setText("")
         self.outputLabel.repaint()
 
-    def hide_shop_items(self):
+    def hide_shop_items(self) -> None:
+        """
+        Hide shop items and their respective buttons and labels.
+        """
         self.shopButton.setVisible(True)
         self.cartButton.setVisible(True)
         self.cookieButton.setVisible(False)
@@ -76,7 +83,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
         self.outputLabel.setText("")
         self.outputLabel.repaint()
 
-    def update_output_label(self):
+    def update_output_label(self) -> None:
+        """Update the output label with the current cart contents and total cost."""
         output = []
         total_cost = 0
 
@@ -85,17 +93,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
                 cost = count * self.prices[item]
                 output.append(f'{item.capitalize()} ({count})= ${cost:.2f}')
                 total_cost += cost
-
+                
         if output:
             output.append(f'\nGRAND TOTAL = ${total_cost:.2f}')
         else:
             output.append('Cart empty!')
 
         self.outputLabel.setText('\n'.join(output))
-
-
+        
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+    
